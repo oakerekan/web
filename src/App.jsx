d5c27def9a4d4ccbfb330037454bc4e3d841c01d
Olayinka Akerekan <105251441+oakerekan@users.noreply.github.com>
2023-12-12 04:37:40 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
+  name: "Olayinka Akerekan",
+  title: "Data Analyst and Data Engineer",
+  email: "olayinkagaiusakerekan@gmail.com",
+  gitHub: "oakerekan",
+  instagram: "",
+  linkedIn: "olayinka-akerekan",
+  medium: "olayinkagaiusakerekan",
+  twitter: "_data_alchemist",
   youTube: "Code",
 };
 
